95e4894ec58fbda40129beb8ff92fdf4c90db09a
Adesh Nalpet <adesh.nalpet@PP-C02Z66CALVCG.local>
2020-05-03 15:45:50 +0530

    Set debug to false for prod env

--- appBuilder/settings.py
+++ appBuilder/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm#tie3gmb&4!saieh9r^w#8d_%9-f!0d%w!f_x5ypql5_u5oxq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
